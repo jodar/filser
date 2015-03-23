f9a1781c38069ebac6ba3602fb2802dc48dbca1d
Luan Jodar <luan.jodar@gmaiil.com>
2015-03-23 14:55:55 -0300

    Tela inicial adicionada

--- spec/acceptance/movies_acceptance_spec.rb
+++ spec/acceptance/movies_acceptance_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
-feature "whatever" do
-  scenario "Signing in with correct credentials" do
-    visit '/movies/new'
-    within("#new") do
-      fill_in 'Title', :with => 'Hobbit'
-      fill_in 'Year', :with => '2014'
-      fill_in 'Description', :with => 'testetestettstststts'
-      fill_in 'Status', :with => 'true'
-      fill_in 'Rating', :with => '10'
-    end
+feature "Filmes" do
+  scenario "Cadastrando um novo filme" do
+    visit new_movie_path
+    fill_in 'Title', :with => 'Hobbit'
+    fill_in 'Year', :with => '2014'
+    fill_in 'Description', :with => 'testetestettstststts'
+    page.check('movie[status]')
+    fill_in 'Rating', :with => '10'
     click_button 'Create Movie'
     expect(page).to have_content 'Movie was successfully created.'
   end
